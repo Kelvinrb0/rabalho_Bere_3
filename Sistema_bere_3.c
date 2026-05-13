a2cb00471bf1f1542173a00d9ba5a10d834f769e
Kelvin <kelvinfoz2007@gmail.com>
2026-05-13 17:34:01 -0300

    Atualização

--- Sistema_bere_3.c
+++ Sistema_bere_3.c
@@ -28,17 +28,21 @@ int main() {
 
             if (valor_pago < valor_pendente) {
                 printf("Dinheiro insuficiente para o total. Abatendo R$ %.2f.\n", valor_pago);
-                valor_pendente = valor_pendente - valor_pago;
+                valor_pendente =- valor_pago;
                 lucro_dia = lucro_dia + valor_pago;
             } else {
                 printf("Pagamento aprovado! Troco a devolver: R$ %.2f\n", valor_pago - valor_pendente);
-                lucro_dia = lucro_dia + valor_pendente;
+                lucro_dia =+ valor_pendente;
                 valor_pendente = 0; 
             }
         } 
         else if (forma_pagamento == 2) {
 
-            printf("Debito ou credito? \n");
+            printf("1 - Debito \n");
+            printf("2 - Credito \n");
+            scanf("%d", &forma_do_cartao);
+
+
 
             printf("Passou o cartao? (1 - Sim / 0 - Nao): ");
             scanf("%d", &cartao_aprovado);
@@ -53,8 +57,8 @@ int main() {
                 }
                 
                 printf("Pagamento no cartao abatido com sucesso!\n");
-                valor_pendente = valor_pendente - valor_pago;
-                lucro_dia = lucro_dia + valor_pago;
+                valor_pendente =- valor_pago;
+                lucro_dia =+ valor_pago;
             } else {
                 printf("Cartao recusado! Selecione a forma de pagamento novamente.\n");
             }
@@ -65,11 +69,11 @@ int main() {
 
             if (valor_pago < valor_pendente) {
                 printf("Dinheiro insuficiente para o total. Abatendo R$ %.2f.\n", valor_pago);
-                valor_pendente = valor_pendente - valor_pago;
-                lucro_dia = lucro_dia + valor_pago;
+                valor_pendente =- valor_pago;
+                lucro_dia =+ valor_pago;
             } else {
                 printf("Pagamento aprovado! Troco a devolver: R$ %.2f\n", valor_pago - valor_pendente);
-                lucro_dia = lucro_dia + valor_pendente;
+                lucro_dia =+ valor_pendente;
                 valor_pendente = 0; 
             }
         }
